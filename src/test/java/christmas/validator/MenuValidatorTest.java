a09d4f407ac18df3912ffae72f99dd6226b1bbeb
nature1216 <annekim1216@naver.com>
2023-11-15 21:13:21 +0900

    test: MenuValidator 테스트 추가 메뉴 이름 일치 여부 검사에 대한 테스트 추가

--- src/test/java/christmas/validator/MenuValidatorTest.java
+++ src/test/java/christmas/validator/MenuValidatorTest.java
@@ -37,7 +37,7 @@ public class MenuValidatorTest {
 
     @DisplayName("주문한 메뉴의 개수가 20개 초과이면 예외가 발생한다.")
     @ParameterizedTest
-    @MethodSource("inValidMenuProvider")
+    @MethodSource("inValidNumProvider")
     void validateNums_주문한_메뉴갸_20개_초과이면_예외(List<String> menus) {
         assertThatThrownBy(() -> MenuValidator.validateNums(menus))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -46,23 +46,56 @@ public class MenuValidatorTest {
 
     @DisplayName("주문한 메뉴의 개수가 20개 이하이면 예외가 발생하지 않는다.")
     @ParameterizedTest
-    @MethodSource("validMenuProvider")
+    @MethodSource("validNumProvider")
     void validateNums_주문한_메뉴갸_20개_이하이면_성공(List<String> menus) {
         assertDoesNotThrow(() -> MenuValidator.validateNums(menus));
     }
 
-    static Stream<List<String>> inValidMenuProvider() {
+    @DisplayName("메뉴의 이름이 일치하지 않는 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @MethodSource("invalidNameProvider")
+    void validateNames_메뉴의_이름이_일치하지_않으면_예외(List<String> menus) {
+        assertThatThrownBy(() -> MenuValidator.validateNames(menus))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
+
+    @DisplayName("메뉴의 이름이 일치하는 경우 예외가 발생하지 않는다.")
+    @ParameterizedTest
+    @MethodSource("validNameProvider")
+    void validateNames_메뉴의_이름이_일치하면_성공(List<String> menus) {
+        assertDoesNotThrow(() -> MenuValidator.validateNames(menus));
+    }
+
+    static Stream<List<String>> inValidNumProvider() {
         return Stream.of(
                 Arrays.asList("양송이수프-5", "티본스테이크-6", "제로콜라-5", "초코케이크-5"),
                 List.of("양송이수프-30")
         );
     }
 
-    static Stream<List<String>> validMenuProvider() {
+    static Stream<List<String>> validNumProvider() {
         return Stream.of(
                 Arrays.asList("양송이수프-5", "티본스테이크-5", "제로콜라-5", "초코케이크-5"),
                 List.of("양송이수프-20")
         );
     }
 
+    static Stream<List<String>> invalidNameProvider() {
+        return Stream.of(
+                Arrays.asList("양송이스프-5", "제로콜라-5"),
+                Arrays.asList("티본스테이크-2","제로콕-5"),
+                List.of("비비큐립-2"),
+                List.of("초콜릿케익-5")
+        );
+    }
+
+    static Stream<List<String>> validNameProvider() {
+        return Stream.of(
+                Arrays.asList("양송이수프-5", "제로콜라-5"),
+                Arrays.asList("티본스테이크-2","제로콜라-5"),
+                List.of("바비큐립-2"),
+                List.of("초코케이크-5")
+        );
+    }
 }
