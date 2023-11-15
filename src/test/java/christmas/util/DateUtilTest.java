f742853464222db8a48d1e46e0fe84d93b04b6a7
nature1216 <annekim1216@naver.com>
2023-11-15 19:59:06 +0900

    test: DateUtil 테스트 추가 DateUtil의 inEventPeriod 메서드 테스트 추가

--- src/test/java/christmas/util/DateUtilTest.java
+++ src/test/java/christmas/util/DateUtilTest.java
@@ -1,5 +1,6 @@
 package christmas.util;
 
+import christmas.enumeration.BenefitType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -17,5 +18,17 @@ public class DateUtilTest {
     })
     void dateToDate(int day, LocalDate expected) {
         assertThat(DateUtil.dayToDate(day)).isEqualTo(expected);
+    }
+
+    @DisplayName("날짜가 이벤트 기간에 해당되는지 여부를 판단한다.")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "2023-12-03, WEEKDAY_DISCOUNT, true",
+            "2023-12-26, X_MAS_DISCOUNT, false",
+            "2023-12-25, X_MAS_DISCOUNT, true",
+            "2023-12-31, GIFT_EVENT, true"
+    })
+    void isEventPeriod(LocalDate current, BenefitType benefitType, boolean expected) {
+        assertThat(DateUtil.inEventPeriod(current, benefitType)).isEqualTo(expected);
     }
 }
