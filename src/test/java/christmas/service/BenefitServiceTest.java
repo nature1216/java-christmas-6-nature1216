59544aa70d23d2fa95e11091b50bade6fccc4520
nature1216 <annekim1216@naver.com>
2023-11-15 18:15:22 +0900

    test: benefitService 테스트 추가 benefitService 테스트 추가

--- src/test/java/christmas/service/BenefitServiceTest.java
+++ src/test/java/christmas/service/BenefitServiceTest.java
@@ -8,15 +8,24 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.LocalDate;
 
-import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BenefitServiceTest {
     private final BenefitService benefitService = new BenefitService();
+
+    @DisplayName("할인 전 총주문금액이 10000원을 넘지 않는 경우 혜택이 적용되지 않는다.")
+    @Test
+    void applyBenefit_총주문금액_10000원_이하() {
+        Order order = new Order();
+        order.update(MenuType.TAPAS, 1);
+        order.update(MenuType.ZERO_COKE, 1);
+        Benefit benefit = benefitService.applyBenefit(order, 3, order.calcTotalCost());
+
+        assertThat(benefit.getTotalBenefit()).isEqualTo(0);
+    }
 
     @DisplayName("주말 할인을 적용한다.")
     @ParameterizedTest
@@ -51,9 +60,9 @@ public class BenefitServiceTest {
         order.update(MenuType.T_BONE_STEAK, 1);
         order.update(MenuType.CHOCOLATE_CAKE, 2);
         order.update(MenuType.ZERO_COKE, 1);
+        benefitService.applyWeekDiscount(benefit, order, date);
 
-        assertThat(benefitService.applyWeekDiscount(benefit, order, date).getAmount(BenefitType.WEEKDAY_DISCOUNT))
-                .isEqualTo(expected);
+        assertThat(benefit.getAmount(BenefitType.WEEKDAY_DISCOUNT)).isEqualTo(expected);
     }
 
     @DisplayName("크리스마스 디데이 할인을 적용한다.")
@@ -73,5 +82,24 @@ public class BenefitServiceTest {
         benefitService.applyXmasDiscount(benefit, date);
 
         assertThat(benefit.getAmount(BenefitType.X_MAS_DISCOUNT)).isEqualTo(expected);
+    }
+
+    @DisplayName("특별 할인을 적용한다.")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "2023-12-03, 1000",
+            "2023-12-04, 0",
+            "2023-12-25, 1000"
+    })
+    void applySpecialDiscount(LocalDate date, int expected) {
+        Order order = new Order();
+        Benefit benefit = new Benefit();
+        order.update(MenuType.BARBEQUE_RIP, 1);
+        order.update(MenuType.T_BONE_STEAK, 1);
+        order.update(MenuType.CHOCOLATE_CAKE, 2);
+        order.update(MenuType.ZERO_COKE, 1);
+        benefitService.applySpecialDiscount(benefit, date);
+
+        assertThat(benefit.getAmount(BenefitType.SPECIAL_DISCOUNT)).isEqualTo(expected);
     }
 }
